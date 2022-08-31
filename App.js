541b7f67779d707b69fa0436b46dec7f69e93389
simran-rabblesoft <simran.sharma@rabblesoft.com>
2022-08-31 15:53:34 +0530

    Further code

--- App.js
+++ App.js
@@ -1,94 +1,166 @@
-import React, { useMemo, useState } from 'react';
-import { View, ActivityIndicator } from 'react-native';
-import Toast, { BaseToast, ErrorToast } from 'react-native-toast-message';
-import { color } from './helper/Common/Colors';
-import fonts from './helper/Common/fonts';
-import { MyStack } from './helper/components/MyStack';
-import { LoaderContext, UserProvider } from './helper/utils/context';
+// import React, { useMemo, useState } from 'react';
+// import { View, ActivityIndicator } from 'react-native';
+// import Toast, { BaseToast, ErrorToast } from 'react-native-toast-message';
+// import { color } from './helper/Common/Colors';
+// import fonts from './helper/Common/fonts';
+// import { MyStack } from './helper/components/MyStack';
+// import { LoaderContext, UserProvider } from './helper/utils/context';
 
- const App = () => {
-  const [isShowLoader, setIsShowLoader] = useState(false);
-  const loaderContext = useMemo(
-    () => ({
-      showLoader: async () => {
-        try {
-          setIsShowLoader(true);
-        } catch (e) {
-          console.log(e);
+//  const App = () => {
+//   const [isShowLoader, setIsShowLoader] = useState(false);
+//   const loaderContext = useMemo(
+//     () => ({
+//       showLoader: async () => {
+//         try {
+//           setIsShowLoader(true);
+//         } catch (e) {
+//           console.log(e);
           
-        }
+//         }
+//       },
+//       hideLoader: async () => {
+//         try {
+//           setIsShowLoader(false);
+//         } catch (e) {
+//           console.log(e);
+//         }
+//       },
+//     }),
+//     [],
+//   );
+//   const toastConfig = {
+   
+//     success: (props) => (
+//       <BaseToast
+//         {...props}
+//         style={{ borderLeftColor: color.green, backgroundColor: color.green }}
+//         contentContainerStyle={{ paddingHorizontal: 15, backgroundColor: color.green, margin: 4 }}
+//         text1Style={{
+//           fontSize: 14,
+//           fontFamily: fonts.semiBold,
+//           color: color.white
+//         }}
+//       />
+//     ),
+//     error: (props) => (
+//       <ErrorToast
+//         {...props}
+//         style={{ borderLeftColor: color.darkRed, backgroundColor: color.darkRed }}
+//         contentContainerStyle={{ paddingHorizontal: 12, backgroundColor: color.darkRed, margin: 4 }}
+//         text1Style={{
+//           fontSize: 14,
+//           fontFamily: fonts.semiBold,
+//           color: color.white,
+//         }}
+//         text2Style={{
+//           fontSize: 14,
+//           fontFamily: fonts.semiBold,
+//           color: color.white,
+//         }}
+//       />
+//     ),
+   
+//     tomatoToast: ({ text1, props }) => (
+//       <View style={{ height: 60, width: '100%', backgroundColor: 'tomato' }}>
+//         <Text>{text1}</Text>
+//         <Text>{props.uuid}</Text>
+//       </View>
+//     )
+//   };
+   
+//   return (
+//     <LoaderContext.Provider value={loaderContext}>
+//       <UserProvider>
+//       <MyStack/>
+//       {isShowLoader && (
+//           <View
+//             style={{
+//               width: '100%',
+//               height: '100%',
+//               position: 'absolute',
+//               justifyContent: 'center',
+//               alignItems: 'center',
+//               zIndex: 1,
+//               backgroundColor:"rgba(0,0,0,0.5)"
+//             }}>
+//             <ActivityIndicator size="large" color={color.titleBlack} />
+//           </View>
+//         )}
+//       </UserProvider>
+//     <Toast config={toastConfig} />
+//     </LoaderContext.Provider>
+//   );
+// };
+// export default App;
+
+
+import React, { useRef } from "react";
+import { Animated, View, StyleSheet, PanResponder, Text } from "react-native";
+import MyComponent from "./Screens/SignUp";
+
+const App = () => {
+  const pan = useRef(new Animated.ValueXY()).current;
+
+  const panResponder = useRef(
+    PanResponder.create({
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderGrant: () => {
+        pan.setOffset({
+          x: pan.x._value,
+          y: pan.y._value
+        });
       },
-      hideLoader: async () => {
-        try {
-          setIsShowLoader(false);
-        } catch (e) {
-          console.log(e);
-        }
-      },
-    }),
-    [],
-  );
-  const toastConfig = {
-   
-    success: (props) => (
-      <BaseToast
-        {...props}
-        style={{ borderLeftColor: color.green, backgroundColor: color.green }}
-        contentContainerStyle={{ paddingHorizontal: 15, backgroundColor: color.green, margin: 4 }}
-        text1Style={{
-          fontSize: 14,
-          fontFamily: fonts.semiBold,
-          color: color.white
-        }}
-      />
-    ),
-    error: (props) => (
-      <ErrorToast
-        {...props}
-        style={{ borderLeftColor: color.darkRed, backgroundColor: color.darkRed }}
-        contentContainerStyle={{ paddingHorizontal: 12, backgroundColor: color.darkRed, margin: 4 }}
-        text1Style={{
-          fontSize: 14,
-          fontFamily: fonts.semiBold,
-          color: color.white,
-        }}
-        text2Style={{
-          fontSize: 14,
-          fontFamily: fonts.semiBold,
-          color: color.white,
-        }}
-      />
-    ),
-   
-    tomatoToast: ({ text1, props }) => (
-      <View style={{ height: 60, width: '100%', backgroundColor: 'tomato' }}>
-        <Text>{text1}</Text>
-        <Text>{props.uuid}</Text>
-      </View>
-    )
-  };
-   
+      onPanResponderMove: Animated.event(
+        [
+          null,
+          { dx: pan.x, dy: pan.y }
+        ]
+      ),
+      onPanResponderRelease: () => {
+        pan.flattenOffset();
+      }
+    })
+  ).current;
+
   return (
-    <LoaderContext.Provider value={loaderContext}>
-      <UserProvider>
-      <MyStack/>
-      {isShowLoader && (
-          <View
-            style={{
-              width: '100%',
-              height: '100%',
-              position: 'absolute',
-              justifyContent: 'center',
-              alignItems: 'center',
-              zIndex: 1,
-              backgroundColor:"rgba(0,0,0,0.5)"
-            }}>
-            <ActivityIndicator size="large" color={color.titleBlack} />
-          </View>
-        )}
-      </UserProvider>
-    <Toast config={toastConfig} />
-    </LoaderContext.Provider>
+    // <View style={styles.container}>
+    //   <Text style={styles.titleText}>Drag this box!</Text>
+    //   <Animated.View
+    //     style={{
+    //       transform: [{ translateX: pan.x }, { translateY: pan.y },
+    //         {
+    //           rotate: pan.x.interpolate({
+    //             inputRange: [-200, 0, 200],
+    //             outputRange: ["-30deg", "0deg", "30deg"],
+    //           }),
+    //         }]
+    //     }}
+    //     {...panResponder.panHandlers}
+    //   >
+    //     <View style={styles.box} />
+    //   </Animated.View>
+    // </View>
+    <MyComponent/>
   );
-};
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  titleText: {
+    fontSize: 14,
+    lineHeight: 24,
+    fontWeight: "bold"
+  },
+  box: {
+    height: 150,
+    width: 150,
+    backgroundColor: "blue",
+    borderRadius: 5
+  }
+});
+
 export default App;
--- Screens/SignUp.js
+++ Screens/SignUp.js
@@ -1,149 +1,238 @@
-import React, { useState, useEffect } from "react";
-import { useNavigation } from "@react-navigation/native";
-import { Formik } from "formik";
-import * as yup from 'yup';
-import { ScrollView, Text, View } from 'react-native';
-import { SafeAreaView } from "react-native-safe-area-context";
-import { color } from "../helper/Common/Colors";
-import CustomTextField from "../helper/components/CustomTextField";
-import { MainButton } from "../helper/components/mainButton";
-import { TextButton } from "../helper/components/TextButton";
-import { AuthStyle } from "../helper/CustomStyle/AuthStyle";
-import { CustomStyling } from "../helper/CustomStyle/CustomStyling";
+// import React, { useState, useEffect } from "react";
+// import { useNavigation } from "@react-navigation/native";
+// import { Formik } from "formik";
+// import * as yup from 'yup';
+// import { ScrollView, Text, View } from 'react-native';
+// import { SafeAreaView } from "react-native-safe-area-context";
+// import { color } from "../helper/Common/Colors";
+// import CustomTextField from "../helper/components/CustomTextField";
+// import { MainButton } from "../helper/components/mainButton";
+// import { TextButton } from "../helper/components/TextButton";
+// import { AuthStyle } from "../helper/CustomStyle/AuthStyle";
+// import { CustomStyling } from "../helper/CustomStyle/CustomStyling";
 
-const SignUpView = ({navigation=useNavigation()}) => {
-    const [isJobSeeker, setIsJobSeeker] = useState(true);
-    const [showErrorMsg, setShowErrMsg] = useState(false);
+// const SignUpView = ({navigation=useNavigation()}) => {
+//     const [isJobSeeker, setIsJobSeeker] = useState(true);
+//     const [showErrorMsg, setShowErrMsg] = useState(false);
 
-    const loginValidationSchema = yup.object().shape({
-        name: yup
-            .string()
-            .required('Please enter name'),
-        email: yup
-            .string()
-            .email("Please enter valid email")
-            .required('Please enter email'),
-        password: yup
-            .string()
-            .min(6, ({ min }) => `Please enter password of at least ${min} characters`)
-            .required('Please enter password'),
-        confirmPassword: yup
-            .string()
-            .required('Please re-enter password')
-            .oneOf([yup.ref('password'), null], 'Please enter confirm password same as password'),
-    });
+//     const loginValidationSchema = yup.object().shape({
+//         name: yup
+//             .string()
+//             .required('Please enter name'),
+//         email: yup
+//             .string()
+//             .email("Please enter valid email")
+//             .required('Please enter email'),
+//         password: yup
+//             .string()
+//             .min(6, ({ min }) => `Please enter password of at least ${min} characters`)
+//             .required('Please enter password'),
+//         confirmPassword: yup
+//             .string()
+//             .required('Please re-enter password')
+//             .oneOf([yup.ref('password'), null], 'Please enter confirm password same as password'),
+//     });
   
-    useEffect(() => {
-        setShowErrMsg(false);
-    }, []); 
+//     useEffect(() => {
+//         setShowErrMsg(false);
+//     }, []); 
 
-    return(
-        <Formik
-            validationSchema={loginValidationSchema}
-            initialValues={{ name: '', email: '', password: '', confirmPassword: '' }}
-            onSubmit={values => {
-                navigation.goBack();
-            }}
-        >
-        {({handleChange, handleBlur, handleSubmit, values, errors, isValid,}) => (
-        <View style={{backgroundColor: color.mainColor, flex: 1}}>
-            <Text style={AuthStyle.viewTitle}>Signup</Text>
-            <View style={{flexDirection: "row", marginTop: 40, marginBottom: 8, marginHorizontal: 40}}>
-                <View style={{width: "50%"}}>
-                <TextButton
-                    text={"Job Seeker"}
-                    textStyle={{color: color.white, textAlign: "center"}}
-                    showUnderLine={isJobSeeker}
-                    onPress={() => {
-                        setShowErrMsg(false);
-                        setIsJobSeeker(true);
-                    }}
-                />
-                </View>
-                <View style={{width: "50%"}}>
-                <TextButton
-                    text={"Recruiter"}
-                    textStyle={{color: color.white, textAlign: "center"}}
-                    showUnderLine={(!isJobSeeker)}
-                    viewStyle={{marginLeft: 32}}
-                    onPress={() => {
-                        setShowErrMsg(false);
-                        setIsJobSeeker(false);
-                    }}
-                />
-                </View>
-                <View style={{width: "50%", alignContent: "center"}}></View>
-            </View>
-        <ScrollView style={CustomStyling.curveViewStyle}>
-            {(!isJobSeeker) ? 
-                <CustomTextField 
-                    pickerLabel="Company Name"
-                    placeholder={"Enter company name"}
-                    defaultValue={""}
-                    containerStyle={{marginTop: 32}}
-                    onTextChange={handleChange("name")}
-                    errorText={errors.name}
-                    showError={ showErrorMsg && errors.name }
-                />
-                : 
-                <CustomTextField 
-                    pickerLabel="Full Name"
-                    placeholder={"Enter Full Name"}
-                    defaultValue={""}
-                    containerStyle={{marginTop: (isJobSeeker) ? 32:0}}
-                    onTextChange={handleChange("name")}
-                    errorText={errors.name}
-                    showError={ showErrorMsg && errors.name }
-                />
+//     return(
+//         <Formik
+//             validationSchema={loginValidationSchema}
+//             initialValues={{ name: '', email: '', password: '', confirmPassword: '' }}
+//             onSubmit={values => {
+//                 navigation.goBack();
+//             }}
+//         >
+//         {({handleChange, handleBlur, handleSubmit, values, errors, isValid,}) => (
+//         <View style={{backgroundColor: color.mainColor, flex: 1}}>
+//             <Text style={AuthStyle.viewTitle}>Signup</Text>
+//             <View style={{flexDirection: "row", marginTop: 40, marginBottom: 8, marginHorizontal: 40}}>
+//                 <View style={{width: "50%"}}>
+//                 <TextButton
+//                     text={"Job Seeker"}
+//                     textStyle={{color: color.white, textAlign: "center"}}
+//                     showUnderLine={isJobSeeker}
+//                     onPress={() => {
+//                         setShowErrMsg(false);
+//                         setIsJobSeeker(true);
+//                     }}
+//                 />
+//                 </View>
+//                 <View style={{width: "50%"}}>
+//                 <TextButton
+//                     text={"Recruiter"}
+//                     textStyle={{color: color.white, textAlign: "center"}}
+//                     showUnderLine={(!isJobSeeker)}
+//                     viewStyle={{marginLeft: 32}}
+//                     onPress={() => {
+//                         setShowErrMsg(false);
+//                         setIsJobSeeker(false);
+//                     }}
+//                 />
+//                 </View>
+//                 <View style={{width: "50%", alignContent: "center"}}></View>
+//             </View>
+//         <ScrollView style={CustomStyling.curveViewStyle}>
+//             {(!isJobSeeker) ? 
+//                 <CustomTextField 
+//                     pickerLabel="Company Name"
+//                     placeholder={"Enter company name"}
+//                     defaultValue={""}
+//                     containerStyle={{marginTop: 32}}
+//                     onTextChange={handleChange("name")}
+//                     errorText={errors.name}
+//                     showError={ showErrorMsg && errors.name }
+//                 />
+//                 : 
+//                 <CustomTextField 
+//                     pickerLabel="Full Name"
+//                     placeholder={"Enter Full Name"}
+//                     defaultValue={""}
+//                     containerStyle={{marginTop: (isJobSeeker) ? 32:0}}
+//                     onTextChange={handleChange("name")}
+//                     errorText={errors.name}
+//                     showError={ showErrorMsg && errors.name }
+//                 />
+//             }
+//            <CustomTextField 
+//                 pickerLabel="Email"
+//                 placeholder={"Enter email"}
+//                 defaultValue={""}
+//                 onTextChange={handleChange("email")}
+//                 errorText={errors.email}
+//                 showError={ showErrorMsg && errors.email }
+//             />
+//             <CustomTextField 
+//                 pickerLabel="Password"
+//                 placeholder={"Enter password"}
+//                 defaultValue={""}
+//                 isPasswordField={true}
+//                 onTextChange={handleChange("password")}
+//                 errorText={errors.password}
+//                 showError={ showErrorMsg && errors.password }
+//             />
+//             <CustomTextField 
+//                 pickerLabel="Confirm Password"
+//                 placeholder={"Re-enter password"}
+//                 defaultValue={""}
+//                 isPasswordField={true}
+//                 onTextChange={handleChange("confirmPassword")}
+//                 errorText={errors.confirmPassword}
+//                 showError={ showErrorMsg && errors.confirmPassword }
+//             />
+//             <MainButton 
+//                 text={"SignUp"}
+//                 onPress={() => {
+//                     (isValid) ? handleSubmit() : setShowErrMsg(true);
+//                 }}
+//                 // disabled={!isValid}
+//             />
+            
+//             <View style={{flexDirection: "row", alignSelf: "center", marginTop: 24, marginBottom: 48}}>
+//                 <Text style={CustomStyling.regular16Text}>Already have an account? </Text>
+//                 <TextButton 
+//                     text={"Login Here"}
+//                     onPress={() => navigation.goBack()}
+//                 />
+//             </View>
+            
+//         </ScrollView>
+//        </View>
+//         )}
+//         </Formik>
+//     );
+
+// };
+
+// export default SignUpView;
+
+import React, { Component } from 'react'; 
+import { StyleSheet, View, Dimensions, PanResponder, Animated } from 'react-native';
+
+export default class MyComponent extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            offset          : 0,
+            topHeight       : 40, // min height for top pane header
+            bottomHeight    : 40, // min height for bottom pane header,
+            deviceHeight    : Dimensions.get('window').height,
+            isDividerClicked: false,
+
+            pan             : new Animated.ValueXY()
+        }
+
+    }
+
+    componentWillMount() {
+        this._panResponder = PanResponder.create({
+            onMoveShouldSetResponderCapture: () => true,
+            onMoveShouldSetPanResponderCapture: () => true,
+
+            // Initially, set the Y position offset when touch start
+            onPanResponderGrant: (e, gestureState) => {
+                this.setState({
+                    offset: e.nativeEvent.pageY,
+                    isDividerClicked: true
+                })
+            },
+
+            // When we drag the divider, set the bottomHeight (component state) again.
+            onPanResponderMove: (e, gestureState) => {
+
+                this.setState({
+                    bottomHeight    : gestureState.moveY > (this.state.deviceHeight - 40) ? 40 : this.state.deviceHeight - gestureState.moveY,
+                    offset: e.nativeEvent.pageY
+                })
+            },
+
+            onPanResponderRelease: (e, gestureState) => {
+                // Do something here for the touch end event
+                this.setState({
+                    offset: e.nativeEvent.pageY,
+                    isDividerClicked: false
+                })
             }
-           <CustomTextField 
-                pickerLabel="Email"
-                placeholder={"Enter email"}
-                defaultValue={""}
-                onTextChange={handleChange("email")}
-                errorText={errors.email}
-                showError={ showErrorMsg && errors.email }
-            />
-            <CustomTextField 
-                pickerLabel="Password"
-                placeholder={"Enter password"}
-                defaultValue={""}
-                isPasswordField={true}
-                onTextChange={handleChange("password")}
-                errorText={errors.password}
-                showError={ showErrorMsg && errors.password }
-            />
-            <CustomTextField 
-                pickerLabel="Confirm Password"
-                placeholder={"Re-enter password"}
-                defaultValue={""}
-                isPasswordField={true}
-                onTextChange={handleChange("confirmPassword")}
-                errorText={errors.confirmPassword}
-                showError={ showErrorMsg && errors.confirmPassword }
-            />
-            <MainButton 
-                text={"SignUp"}
-                onPress={() => {
-                    (isValid) ? handleSubmit() : setShowErrMsg(true);
-                }}
-                // disabled={!isValid}
-            />
-            
-            <View style={{flexDirection: "row", alignSelf: "center", marginTop: 24, marginBottom: 48}}>
-                <Text style={CustomStyling.regular16Text}>Already have an account? </Text>
-                <TextButton 
-                    text={"Login Here"}
-                    onPress={() => navigation.goBack()}
-                />
+        });
+    }
+
+
+    render() {
+        return ( 
+            <View style={styles.content}>
+
+                {/* Top View */}
+                <Animated.View 
+                    style       = {[{backgroundColor: 'pink', minHeight: 40, flex: 1}, {height: this.state.topHeight}]}
+
+                >
+                </Animated.View>
+
+                {/* Divider */}
+                <View 
+                    style={[{height: 10}, this.state.isDividerClicked ? {backgroundColor: '#666'} : {backgroundColor: '#e2e2e2'}]} 
+                    {...this._panResponder.panHandlers}
+                >
+                </View>
+
+                {/* Bottom View */}
+                <Animated.View 
+                    style={[{backgroundColor: 'green', minHeight: 40}, {height: this.state.bottomHeight}]} 
+
+                >
+                </Animated.View>
             </View>
-            
-        </ScrollView>
-       </View>
-        )}
-        </Formik>
-    );
+        )
+    }
+}
 
-};
-
-export default SignUpView;
+const styles = StyleSheet.create({
+    content         : {
+        flex        : 1,
+        flexDirection: 'column'
+    },
+})
